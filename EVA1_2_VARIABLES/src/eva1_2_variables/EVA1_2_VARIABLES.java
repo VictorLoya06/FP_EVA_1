2508a4a9a2f621e25f114e3d304244ef43ab4af6
VictorLoya06 <L24550312@chihuahua2.tecnm.mx>
2024-08-27 16:20:37 -0600

    EVA1_2_VARIABLES

---
+++ EVA1_2_VARIABLES/src/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_2_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo!!");//UN TEXTO
+        System.out.println(100);//UN NUMERO
+        System.out.println('c');//UN CARACTER
+        System.out.println(true);// valores logicos
+        System.out.println(false);
+        System.out.println(1000.52);// numeros con fraccion (con punto flotante)
+        // TIPOS DE DATOS
+        //ENTEROS
+        //int
+        //byte
+        //short
+        //PUNTO FLOTANTE
+        //double
+        //float
+        //TEXTO
+        //string
+        //CARACTER
+        //char
+        //LOGICOS(true false)
+        //boolean
+        
+        //creacion y uso de variable
+        int edad; //Declaro una variable de tipo entero ,llamado
+                  //edad
+        //double edad; // NO PODEMOS TENER DOS VARIABLES CON EL MISMO NOMBRE
+        String nombre;
+        String apellido;
+        double promedio;
+        
+        edad = 18;
+        System.out.println(edad);
+    }
+    
+}
