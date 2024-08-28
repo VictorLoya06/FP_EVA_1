5a94f610ff00aea2657642c326bbce6013098f51
VictorLoya06 <L24550312@chihuahua2.tecnm.mx>
2024-08-28 16:16:14 -0600

    EVA1_3_AUTOMOVIL

---
+++ EVA1_3_AUTOMOVIL/src/eva1_3_automovil/EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_3_automovil;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+        
+        // ASIGNAR VALORES
+        marca="Ford";
+        modelo="mustang";
+        year=2022;
+        precio=999000.1;
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+
+    }
+    
+}
