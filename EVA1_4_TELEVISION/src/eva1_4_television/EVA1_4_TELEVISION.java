0832af971a3c53200f1c0f1e22bc44dc270deb3a
VictorLoya06 <L24550312@chihuahua2.tecnm.mx>
2024-08-28 16:16:39 -0600

    EVA1_4_TELEVISION

---
+++ EVA1_4_TELEVISION/src/eva1_4_television/EVA1_4_TELEVISION.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_4_television;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_TELEVISION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    String Marca;
+    String Modelo;
+    int tamaño;
+    double Precio;
+    String tecno;
+    double descuento;       
+    // ASIGNAR VALORES
+    
+     Marca="Samsung Smart TV";
+     Modelo="Crytal UHD";
+     tamaño=55;
+     tecno="LED";
+     Precio=6990.00;
+     descuento=30;
+     
+        System.out.println(Marca);
+        System.out.println(Modelo);
+        System.out.println(tamaño);
+        System.out.println(tecno);
+        System.out.println(Precio);
+        System.out.println(descuento);
+    }
+   
+
+    
+        
+    
+}
