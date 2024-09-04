455fd5dc1ea44683da7601197556c49442b3c689
VictorLoya06 <L24550312@chihuahua2.tecnm.mx>
2024-09-04 16:51:46 -0600

    EVA1_8_OPERACIONES2

---
+++ EVA1_8_OPERACIONES2/src/eva1_8_operaciones2/EVA1_8_OPERACIONES2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_8_operaciones2;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_8_OPERACIONES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre ="hugo";
+        String apellido ="loya";
+        String nombreCom;
+        //cadenas detexto,la operacion se llama concatenacion
+        //-->unir cadenas de texto
+        nombreCom = nombre +" "+ apellido;
+        System.out.println(nombreCom);
+        
+        System.out.println("El nombre es:" + nombre);
+        System.out.println("El apellido es:" + apellido);
+        System.out.println("El nombre completo es: "+ nombre +" "+ apellido);
+    }
+    
+    
+}
