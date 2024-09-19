13933be08cbb2b3c11387e57f5365a7dedcc8dfa
VictorLoya06 <L24550312@chihuahua2.tecnm.mx>
2024-09-19 15:37:54 -0600

    EVA1_12_ CALIFAS

---
+++ EVA1 12 CALIFAS/src/eva1/pkg12/califas/EVA1_12_CALIFAS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1.pkg12.califas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author noziv
+ */
+public class EVA1_12_CALIFAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner captu;
+        captu = new Scanner(System.in);
+        int califa;
+        System.out.println("calificacon");
+        califa = captu.nextInt();
+         if (califa >=70)
+         
+         { System.out.println("Acreditas");}
+         else
+         
+         {System.out.println("N/A");}
+        
+    }
+    
+}
